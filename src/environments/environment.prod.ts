c4749f48f229d6f36fab1c8dc58389ef2488f8e1
mosapride <mosapride01@gmail.com>
2021-07-04 14:48:30 +0900

    change : v-kurbore -> freeboll

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  host: 'https://hako.v-kurore.com',
+  host: 'https://freeboll.com',
   production: true,
-  apiUrl: 'https://api.v-kurore.com/api/v1/',
-  imageUrl : 'https://api.v-kurore.com/assets/'
+  apiUrl: 'https://api.freeboll.com/api/v1/',
+  imageUrl : 'https://api.freeboll.com/assets/'
 };

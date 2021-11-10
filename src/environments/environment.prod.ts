50e6a6042c94daff68dda987a59bf35ef4743739
mosapride <mosapride01@gmail.com>
2021-11-10 09:27:21 +0900

    change : api.freeboll.com -> freeboll.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   host: 'https://freeboll.com',
   production: true,
-  apiUrl: 'https://api.freeboll.com/api/v1/',
-  imageUrl : 'https://api.freeboll.com/assets/'
+  apiUrl: 'https://freeboll.com/api/v1/',
+  imageUrl : 'https://freeboll.com/assets/'
 };
